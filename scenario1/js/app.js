d6a9c5185d34bd1be35bdb384dd1cbb7d6ecad03
jr005 <rubenjr005@gmail.com>
2022-02-02 22:07:23 -0600

    created a function and popup for email validation

--- scenario1/js/app.js
+++ scenario1/js/app.js
@@ -3,28 +3,69 @@
 
 // DOCUMENT - HAVE ACCESS TO THE WHOLE DOM DOCUMENT BY USING THE WORD "DOCUMENT" IN CONSOLE LOG
 
-// GOOGLE - FIND ELEMENT BY CLASS
 
-// ANONYMOUS FUNCTION
+// FUNCTION TO LOAD POP UP WINDOW
+
 window.onload = function () {
 
-    // VARIABLE TO GET THE CLASS NAME FOR POPUP MODAL
+    // VARIABLE - MAKE POP INTO BOOLEON
+    let emailState = false
+
+    // VARIABLE - GET THE CLASS NAME FOR POPUP MODAL
     let emailModal = document.getElementsByClassName('email-modal') [0]
 
-    // VARIABLE TO GET THE CLASS NAME TO CLOSE MODAL
+    // VARIABLE - GET THE CLASS NAME TO CLOSE MODAL
     let closeModal = document.getElementsByClassName('email-modal__close-btn') [0]
 
-    console.log(closeModal)
+    // VARIABLE - INPUT EMAIL
+    let emailInput = document.getElementsByClassName('email-modal__input') [0]
 
-    console.log(emailModal)
-
-    // FUNCTION FOR WHEN MOUSE LEAVES PAGE
-    document.addEventListener('mouseleave', () => {
+    // VARIABLE - EMAIL BUTTON
+    let emailButton = document.getElementsByClassName('email-modal__button') [0]
 
 
-        // VARIABLE - ADDING A CLASS
-        emailModal.classList.add('email-modal--visible');
-        
+    // EMAIL IS VALID FUNCTION
+    function emailIsValid (email) {
+        return /\S+@\S+\.\S+/.test(email)
+      }
+
+    // EMAIL VALIDATION INPUT BUTTON FUNCTION
+    emailButton.addEventListener('click', () => {
+        if (emailIsValid(emailInput.value)) {
+
+            console.log(emailInput.value)
+        } else {
+            alert('this is not a valid email')
+        }
     })
+
+
+
+    // VARIABLE - SHOW MODAL FUNCTION
+    let showModal = () => {
+    // USEING IF TO MAKE SURE MODAL DOES NOT OPEN AGAIN AFTER IT GETS CLOSED OUT
+        if(emailState == false) {
+            // VARIABLE - ADDING A CLASS
+            emailModal.classList.add('email-modal--visible');
+            emailState = true
+        }
+    }
+
+    closeModal.addEventListener('click', () => {
+        emailModal.classList.remove('email-modal--visible');
+    } )
+                // FUNCTION FOR WHEN MOUSE LEAVES PAGE
+        document.body.addEventListener('mouseleave', () => {
+            // TRIGGER THE FUNCTION
+            showModal();
+        })
+
+    
     console.log(window.document)
 }
+
+    // USE CONSOLE LOG TO SHOW WHAT THE CLOSEMODAL VARIABLE IS DOING
+    // console.log(closeModal)
+    
+    // USE CONSOLE LOG TO SHOW WHAT THE EMAILMODAL VARIABLE IS DOING
+    // console.log(emailModal)

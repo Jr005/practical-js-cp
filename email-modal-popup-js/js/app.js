48507683d389bea446f078837a6ef7a8f986a104
jr005 <rubenjr005@gmail.com>
2022-02-05 15:04:24 -0600

    I decided to change the name of one of the projects folders from scenario1 to email modal popup js

---
+++ email-modal-popup-js/js/app.js
@@ -0,0 +1,108 @@
+// .ONLOAD - ONCE EVERYTHING HAS LOADED THEN WERE GOING TO RUN OUR JAVASCRIPT
+
+// DOCUMENT - HAVE ACCESS TO THE WHOLE DOM DOCUMENT BY USING THE WORD "DOCUMENT" IN CONSOLE LOG
+
+// FUNCTION TO LOAD POP UP WINDOW
+
+window.onload = function () {
+  let emailState = false;
+  let emailModal = document.getElementsByClassName("email-modal")[0];
+  let closeButton = document.getElementsByClassName(
+    "email-modal__close-btn"
+  )[0];
+  let emailInput = document.getElementsByClassName("email-modal__input")[0];
+  let emailButton = document.getElementsByClassName("email-modal__button")[0];
+  let thankContainer = document.getElementsByClassName("email-thank")[0];
+  let declineOffer = document.getElementsByClassName(
+    "email-modal__decline-offer"
+  )[0];
+
+  // VARIABLE - SHOW MODAL FUNCTION
+  let showModal = () => {
+    // USEING IF STATEMENT TO MAKE SURE MODAL DOES NOT OPEN AGAIN AFTER IT GETS CLOSED OUT BY USING BOOLEAN (TURNING EMAIL STATE FROM FALSE TO TRUE)
+    if (emailState == false) {
+      // VARIABLE - ADDING A CLASS
+      emailModal.classList.add("email-modal--visible");
+      emailState = true;
+    }
+  };
+
+  // ADD ERRORS FUNCTION
+
+  let addErrors = () => {
+    document
+      .getElementsByClassName("email-modal__form-group")[0]
+      .classList.add("email-modal__form-group--error");
+
+    document
+      .getElementsByClassName("email-modal__error-message")[0]
+      .classList.add("email-modal__error-message--active");
+  };
+
+  // REMOVE ERRORS FUNCTION
+
+  let removeErrors = () => {
+    document
+      .getElementsByClassName("email-modal__form-group")[0]
+      .classList.remove("email-modal__form-group--error");
+
+    document
+      .getElementsByClassName("email-modal__error-message")[0]
+      .classList.remove("email-modal__error-message--active");
+  };
+
+  let showThankMessage = () => {
+    thankContainer.classList.add("email-thank--success");
+    setTimeout(() => {
+      closeModal();
+    }, 3000);
+  };
+
+  closeButton.addEventListener("click", () => {
+    closeModal();
+  });
+
+  // REMOVE ERROR WARNING AFTER USER CLICKS ON INPUT
+  emailInput.addEventListener("click", () => {
+    // REMOVE ERRORS FUNCTION
+    removeErrors();
+  });
+
+  // EMAIL IS VALID FUNCTION
+  function emailIsValid(email) {
+    return /\S+@\S+\.\S+/.test(email);
+  }
+
+  // FUNCTION TO CLOSE MODAL
+  let closeModal = () => {
+    emailModal.classList.remove("email-modal--visible");
+  };
+
+  // EMAIL VALIDATION INPUT BUTTON FUNCTION
+  emailButton.addEventListener("click", () => {
+    if (emailIsValid(emailInput.value)) {
+      showThankMessage();
+    } else {
+      addErrors();
+    }
+  });
+
+  // FUNCTION TO CLOSE WINDOW WHEN THEY CLICK ON DECLINE OFFER LINK
+  declineOffer.addEventListener("click", () => {
+    closeModal();
+  });
+
+  // FUNCTION FOR WHEN MOUSE LEAVES PAGE
+  document.body.addEventListener("mouseleave", () => {
+    // TRIGGER THE FUNCTION
+    showModal();
+  });
+
+  console.log(window.document);
+};
+
+// USE CONSOLE LOG TO SHOW WHAT THE CLOSEMODAL VARIABLE IS DOING
+// console.log(closeModal)
+
+// USE CONSOLE LOG TO SHOW WHAT THE EMAILMODAL VARIABLE IS DOING
+// console.log(emailModal)
